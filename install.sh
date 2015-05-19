3c327b05c82b6f01e9161940c1e7eca574e789c5
Jose Rodriguez <jmrbcu@gmail.com>
2015-05-19 01:02:28 -0500

    Added upgrade and fixed repo url

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install ssh ruby mc htop redis-server redis-tools git python-pip supervisor
 sudo cp /etc/redis/redis.conf /etc/redis/redis.conf.save
 sudo sed -i 's/bind 127.0.0.1/bind 0.0.0.0/g' /etc/redis/redis.conf
@@ -11,7 +12,7 @@ sudo git clone https://github.com/marians/rebrow.git /usr/local/rebrow
 sudo pip install -r /usr/local/rebrow/requirements.txt
 
 # io server
-git clone https://github.com/io_server.git /usr/local/io_server
+git clone https://github.com/jmrbcu/io_server.git /usr/local/io_server
 sudo pip install -r /usr/local/io_server/requirements.txt
 
 # configure supervisor
