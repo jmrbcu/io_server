f934e044dd726e99bc50e1bc3c2e60acbce9605f
Jose Rodriguez <jmrbcu@gmail.com>
2015-05-19 07:49:23 -0500

    Installing pyusb now through pip

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install ssh ruby mc htop redis-server redis-tools git python-pip supervisor python-usb python-serial python-construct python-redis python-hiredis
+sudo apt-get -y install ssh ruby mc htop redis-server redis-tools git python-pip supervisor python-serial python-construct python-redis python-hiredis libusb
 sudo cp /etc/redis/redis.conf /etc/redis/redis.conf.save
 sudo sed -i 's/bind 127.0.0.1/bind 0.0.0.0/g' /etc/redis/redis.conf
 sudo sed -i 's/daemonize yes/daemonize no/g' /etc/redis/redis.conf
@@ -9,7 +9,7 @@ sudo update-rc.d redis-server disable
 
 # redis client
 sudo git clone https://github.com/marians/rebrow.git /usr/local/rebrow
-sudo pip install -r /usr/local/rebrow/requirements.txt
+sudo pip install --pre -r /usr/local/rebrow/requirements.txt
 
 # io server
 git clone https://github.com/jmrbcu/io_server.git /usr/local/io_server
