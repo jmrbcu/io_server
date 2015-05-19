5b8abe9de358d834b1f6ee7a0fbaed07118bb06b
Jose Rodriguez <jmrbcu@gmail.com>
2015-05-19 00:53:51 -0500

    Fixed issue with git not installed

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
-git clone https://github.com/io_server.git /usr/local/io_server
-cd /usr/local/io_server
-
 sudo apt-get -y update
 sudo apt-get -y ssh ruby mc htop redis-server redis-tools git python-pip supervisor
-sudp cp /etc/redis/redis.conf /etc/redis/redis.conf.save
+sudo cp /etc/redis/redis.conf /etc/redis/redis.conf.save
 sudo sed -i 's/bind 127.0.0.1/bind 0.0.0.0/g' /etc/redis/redis.conf
 sudo sed -i 's/daemonize yes/daemonize no/g' /etc/redis/redis.conf
 sudo service redis-server stop
@@ -13,11 +10,12 @@ sudo update-rc.d redis-server disable
 sudo git clone https://github.com/marians/rebrow.git /usr/local/rebrow
 sudo pip install -r /usr/local/rebrow/requirements.txt
 
-# io server dependencies
-sudo pip install -r requirements.txt
+# io server
+git clone https://github.com/io_server.git /usr/local/io_server
+sudo pip install -r /usr/local/io_server/requirements.txt
 
 # configure supervisor
-sudo cp conf/supervisord.conf /etc/supervisor/supervisord.conf
+sudo cp /usr/local/io_server/conf/supervisord.conf /etc/supervisor/supervisord.conf
 sudo service supervisor stop
 sudo service supervisor start
 
